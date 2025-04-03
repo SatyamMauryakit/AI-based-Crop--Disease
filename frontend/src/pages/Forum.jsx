1534e208f9a259e02c871080a13f6c09d5b35db0
Satyam Maurya <144161614+SatyamMauryakit@users.noreply.github.com>
2025-04-03 23:44:39 +0530

    Update Forum.jsx

--- frontend/src/pages/Forum.jsx
+++ frontend/src/pages/Forum.jsx
@@ -16,7 +16,7 @@ function Forum() {
 
   const fetchPosts = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/posts");
+      const response = await fetch("https://ai-based-crop-disease.onrender.com/api/posts");
       const data = await response.json();
       setPosts(data);
     } catch (err) {
